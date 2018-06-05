261d61a7ff2f723f5d24db488dc8114f6d5efe3d
“YOUR <annichiarico.d@gmail.com>
2018-06-05 12:08:37 -0400

    javascript

---
+++ main.js
@@ -0,0 +1,19 @@
+//enter vars
+var one = prompt();
+var two = prompt();
+var three = prompt();
+
+
+// compare numbers numerically
+if((one > two) && (one > three)){
+  console.log(one);
+}
+else if((two > three) && (two > one)){
+  console.log(two);
+}
+ else{
+   console.log(three);
+ }
+
+
+//return the largest number
